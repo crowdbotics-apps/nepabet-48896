75ac41e78203b126e89a32173ebf6816eb62e736
Crowdbotics <team@crowdbotics.com>
2024-07-30 06:07:19 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "nepabet-48896",
+  url: "https://nepabet-48896.botics.co",
+  api: "https://nepabet-48896.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
